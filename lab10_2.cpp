20046e5c6f128308155c262736eb5eb17d581db4
Piicharpa <neuah2515@gmail.com>
2022-12-27 16:06:59 +0700

    complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -22,3 +22,19 @@ int main(){
 	
 	return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+  
+  void printO(int M,int N ){
+  if(M > 0 and N > 0){
+    for(int i = 0; i < M; i++){
+        for(int j = 0; j < N; j++){
+            cout << 'O';
+        }
+        cout << '\n';
+    }
+    }
+  else{cout << "Invalid input";}
+  }
